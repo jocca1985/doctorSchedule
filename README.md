b65c1a75ee05da34bf63e18a20a1afc641458ad8
Jovan <jocca1985@gmail.com>
2020-01-29 13:30:25 +0100

    Update run and sample json

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # doctorSchedule
+
+Install dependencies
+python -m pip install --upgrade --user ortools
+pip install pandas
+
+Run
+python run.py
--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import collections
 import csv
 import pandas as pd
 from collections import defaultdict
-import boto3
 
 class AutoVivification(dict):
     """Implementation of perl's autovivification feature."""
@@ -21,43 +20,36 @@ def main():
     # (3 shifts per day, for 7 days), subject to some constraints (see below).
     # Each nurse can request to be assigned to specific shifts.
     # The optimal assignment maximizes the number of fulfilled shift requests.
-    num_nurses = 13
+    with open('sample.json') as f:
+        data = json.load(f)
+
+    num_nurses = int(data['num_doctors'])
     num_shifts = 1
-    num_days = 7
-    num_rooms = 4
+    num_days = int(data['num_days'])
+    num_rooms = int(data['num_rooms'])
+    constraints = data['constraints']
+    vacations = data['vacation']
     all_nurses = range(num_nurses)
     all_shifts = range(num_shifts)
     all_rooms = range(num_rooms)
     all_days = range(num_days)
-    # shift_requests = [[[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                    [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                   [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                   [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                   [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]],
-    #                   [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0],
-    #                    [0, 0, 0], [0, 0, 0]]]
 
-    room_requests = [ [0]*num_rooms for _ in all_nurses ]
-    for n in all_nurses:
-        if (constraints[n]['room']):
-            room_number = int(constraints[n]['room'])
-            room_requests[n][room_number] = 1
-    
+    room_requests = [ [0]*num_nurses for _ in all_rooms ]
+    # room_requests = [room_requests for _ in all_rooms]
+    for constraint, item in enumerate(constraints):
+        if (constraints[constraint]['doctor']):
+            doctor_number = int(constraints[constraint]['doctor'])
+            # for d in all_days:
+            room_requests[constraint][doctor_number] = 1
+
+    # for i, item in enumerate(vacations):
+    #     start = int(vacations[i]['dayStart'])
+    #     end = int(vacations[i]['dayEnd'])
+    #     for d in range(start,end):
+    #         for r in all_rooms:
+    #             doctor_number = int(vacations[i]['doctor'])
+    #             room_requests[r][d][doctor_number] = 0
+    # print(len(room_requests))
     # Creates the model.
     model = cp_model.CpModel()
 
@@ -78,12 +70,20 @@ def main():
     for n in all_nurses:
         for d in all_days:
             model.Add(sum(shifts[(n, d, r)] for r in all_rooms) <= 1)
+
+    # for i, item in enumerate(vacations):
+    #     start = int(vacations[i]['dayStart'])
+    #     end = int(vacations[i]['dayEnd'])
+    #     for d in range(start,end):
+    #         for r in all_rooms:
+    #             n = int(vacations[i]['doctor'])
+    #             model.Add(shifts[(n,d,r)] == 0)
             
     # room constraints
     # for d in all_days:
     #     for s in all_shifts:
     #         model.Add(shifts[(0, d, s, 0)] == 1)
-    model.Maximize(sum(room_requests[i][r]*shifts[(i, d, r)] for i in range(len(room_requests)) for d in all_days for r in all_rooms))
+    model.Maximize(sum(room_requests[r][i]*shifts[(i, d, r)] for i in range(len(room_requests)) for d in all_days for r in all_rooms))
     # min_shifts_assigned is the largest integer such that every nurse can be
     # assigned at least that number of shifts.
     min_shifts_per_nurse = (num_rooms*num_shifts * num_days) // num_nurses
@@ -104,39 +104,17 @@ def main():
     print("Solved")
     data = AutoVivification()
     for d in all_days:
-        print('Day', d)
         for n in all_nurses:
             for r in all_rooms:
                 if solver.Value(shifts[(n, d, r)]) == 1:
-                    if n<len(room_requests) and room_requests[n][r] == 1:
-                        data["Day"+str(d)]["Room"+str(r)]= constraints[n]['doctor']
-                        print('Nurse', constraints[n]['doctor'], 'works ' , 'in room', r, '(requested).')
-                    else:
-                        data["Day"+str(d)]["Room"+str(r)]=constraints[n]['doctor']
-                        print('Nurse', constraints[n]['doctor'], 'works', 'in room', r, '(not requested).')
-        print()
+                    data["Day"+str(d)][constraints[r]['room']]= n
 
     df = pd.DataFrame.from_dict({(i): data[i] 
                            for i in data.keys() 
                            },orient='index')
-    source = '/tmp/schedule.xls'
-    bucket = 'doctor-schedule'
-    destination = 'schedule.xls'
+    source = './schedule.xls'
     df.to_excel(source, index=True)
-    upload(source,bucket,destination)
-    url = 'https://'+bucket+'.s3.amazonaws.com/'+destination
-    result = {
-        'statusCode':200,
-        'headers':{'Content-Type':'application/json','Access-Control-Allow-Origin':'*','Access-Control-Allow-Headers':'*'},
-        'body':json.dumps({'url': url})
-    }
-    print(result)
-    # Statistics.
-    print()
-    print('Statistics')
-    print('  - Number of shift requests met = %i' % solver.ObjectiveValue(),
-          '(out of', num_nurses * min_shifts_per_nurse, ')')
-    print('  - wall time       : %f s' % solver.WallTime())
+    print(data)
 
 
 if __name__ == '__main__':
